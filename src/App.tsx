c311f4e36fd4fbeb32695654bcba99d66a38a8be
Chris Baird <chrisbaird05@icloud.com>
2019-01-11 11:46:11 -0800

    Working on laout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
+import { Container } from "./Components/BootstrapComponents/Grid";
+import Nav from "./Components/BootstrapComponents/Nav";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <Nav />
+        <Container>
+          <p />
+        </Container>
+      </div>
+    );
   }
 }
 
---
+++ src/Components/BootstrapComponents/Grid.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Container = ({ children }: { children: any }) => (
+  <div className="container">{children}</div>
+);
+
+const Row = ({ children }: { children: any }) => (
+  <div className="row">{children}</div>
+);
+
+const Col = ({ children, size }: { children: any; size: string }) => (
+  <div className={size}>{children}</div>
+);
+
+export { Container, Row, Col };
